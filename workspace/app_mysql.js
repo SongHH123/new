3d045c543d9a3c1d4e04ff374f98c4d30b8c9b27
SongHyeHyeon <1971318@hansung.ac.kr>
2021-08-22 18:07:58 +0900

    fix login function

--- workspace/app_mysql.js
+++ workspace/app_mysql.js
@@ -4,9 +4,6 @@ const fs = require('fs');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 
-
-const app = express();
-
 const connection = mysql.createConnection({
   host:'localhost',
   user:'root',
@@ -15,6 +12,7 @@ const connection = mysql.createConnection({
   port: '3306',
 });
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('html', require('ejs').renderFile);
@@ -27,23 +25,65 @@ app.set('views', './html');
 app.use(express.static('html'));
 app.use(express.static('workspace'));
 
+
 app.listen(4000, () => {
-  console.log(`Example app listening at http://localhost:4000`);
-    connection.connect();
+  console.log(`Example app listening at http://localhost:4000 or http://localhost:4000/tr`);
+  connection.connect();
 });
 
+//학생 서버
 app.get('/', (req, res)=>{
   res.render('index');
 });
 
+app.post('/st_main', (req, res)=>{
+  const body = req.body;
+  var id = parseInt(body.uname);
+
+  connection.query('select * from user where user_id = ? and user_psw = ?',
+  [id ,body.psw], (error, results, fields) =>{
+    if(error || results[0] === undefined) {
+      res.redirect('/');
+    }
+    console.log(results[0]);
+    res.redirect('st_main');
+  });
+});
+
+app.get('/st_main', (req, res)=>{
+  res.render('st_main');
+})
+
+app.get('/st_attendance', (req, res)=>{
+  res.render('st_attendance');
+});
+
+//선생님 서버
 app.get('/tr', (req, res)=>{
   res.render('index_tr');
 });
 
-app.post('/st_main', (req, res)=>{
-  res.render('st_main');
+app.post('/tr_main', (req, res)=>{
+  const body = req.body;
+
+  connection.query('select * from educater where tr_id = ? and tr_pw = ?',
+   [body.uname, body.psw], () =>{
+    res.redirect('tr_main');
+  });
 });
 
-app.post('/tr_main', (req, res)=>{  
+app.get('/tr_main', (req, res)=>{
   res.render('tr_main');
-});
+})
+
+app.get('/tr_student', (req, res)=>{
+  res.render('tr_student');
+})
+
+app.get('/tr_contents', (req, res)=>{
+  res.render('tr_contents');
+})
+
+app.get('/tr_attendance', (req, res)=>{
+  res.render('tr_attendance');
+})
--- workspace/database.js
+++ workspace/database.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql');
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host:'localhost',
   user:'root',
   password:'1234',
@@ -7,15 +7,5 @@ const connection = mysql.createConnection({
   port: '3306',
 });
 
-
-module.exports = {
-    init: function () {
-        return mysql.createConnection(connection);
-    },
-    connect: function(conn) {
-        conn.connect(function(err) {
-            if(err) console.error('mysql connection error : ' + err);
-            else console.log('mysql is connected successfully!');
-        });
-    }
-}
+db.connect();
+module.export = db;
