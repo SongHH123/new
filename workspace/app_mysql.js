469fd6f4236603119e87629ae7a6a37dcefec853
SongHyeHyeon <1971318@hansung.ac.kr>
2021-08-29 22:25:11 +0900

    fix login function and show result by console

--- workspace/app_mysql.js
+++ workspace/app_mysql.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var http = require('http');
 
 const connection = mysql.createConnection({
   host:'localhost',
@@ -54,11 +55,21 @@ app.post('/st_main', (req, res)=>{
 });
 
 app.get('/st_main', (req, res)=>{
+  var user = req.headers.cookie.split('=', 2);
+  user = user[1];
+  if(user == undefined){ res.send("<script>alert('로그인 정보가 없습니다.'); location.href='./';</script>"); }
 
-  res.render('st_main');
-})
+  connection.query('select * from user_sub where user_id = ?',[user], (error, results, fields) => {
+    if(error) throw error;
+    console.log(results);
+    res.render('st_main');
+  });
+});
 
 app.get('/st_attendance', (req, res)=>{
+  var user = req.headers.cookie.split('=', 2);
+  user = user[1];
+  if(user == undefined){ res.send("<script>alert('로그인 정보가 없습니다.'); location.href='./';</script>");  }
   res.render('st_attendance');
 });
 
@@ -70,30 +81,41 @@ app.get('/tr', (req, res)=>{
 
 app.post('/tr_main', (req, res)=>{
   const body = req.body;
-
   connection.query('select * from educater where tr_id = ? and tr_pw = ?',
    [body.uname, body.psw], (error, results, fields) =>{
      if(error || results[0] == undefined)  {
        res.send("<script>alert('아이디와 비밀번호를 다시 확인해주세요.'); location.href='./tr';</script>");
        return error; }else{
-         res.cookie('id', id);
+         res.cookie('id', body.uname);
          res.redirect('tr_main');
        }
   });
 });
 
 app.get('/tr_main', (req, res)=>{
+  var user = req.headers.cookie.split('=', 2);
+  user = user[1];
+  if(user == undefined)  { res.send("<script>alert('로그인 정보가 없습니다.'); location.href='./tr';</script>");  }
   res.render('tr_main');
 })
 
 app.get('/tr_student', (req, res)=>{
+  var user = req.headers.cookie.split('=', 2);
+  user = user[1];
+  if(user == undefined)  { res.send("<script>alert('로그인 정보가 없습니다.'); location.href='./tr';</script>");  }
   res.render('tr_student');
 })
 
 app.get('/tr_contents', (req, res)=>{
+  var user = req.headers.cookie.split('=', 2);
+  user = user[1];
+  if(user == undefined)  { res.send("<script>alert('로그인 정보가 없습니다.'); location.href='./tr';</script>");  }
   res.render('tr_contents');
 })
 
 app.get('/tr_attendance', (req, res)=>{
+  var user = req.headers.cookie.split('=', 2);
+  user = user[1];
+  if(user == undefined)  { res.send("<script>alert('로그인 정보가 없습니다.'); location.href='./tr';</script>");  }
   res.render('tr_attendance');
 })
